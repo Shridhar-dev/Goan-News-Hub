d7740cfc992e4c4fa6ae52936111b2c32bafd062
Shridhar-dev <shridharkamat10@gmail.com>
2020-06-28 22:10:39 +0530

    Clean Code

---
+++ src/Explore.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import fb from './fb.png';
+import yt from './yt.png';
+import tw from './tw.png';
+import './Explore.css';
+
+class Explore extends React.Component{
+    constructor(){
+        super()
+        this.state={
+
+        }
+    }
+    render(){
+        return(
+            <div class="container-fluid graddiv mb-5">
+                <div class="row align-items-center">
+                    <div class="col-12 col-xl-6 col-md-6 col-sm-12">
+                        <div class="pt-2 pb-0 pb-xl-5 pb-md-5 pb-sm-0 display-2">Explore <span class="fad rounded px-3 py-2"><strong>Within</strong></span></div>
+                        <div class="pt-2 grad-des"><h2>Supporting<strong> Amazing </strong>creators all around Goa</h2></div>
+
+                    </div>
+                    <div class="col-12 col-xl-6 col-md-6 col-sm-12">
+                        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                            <div class="carousel-inner align-items-center justify-content-center">
+                                <div class="carousel-item active">
+                                    <i class="fab fa-youtube"></i>
+                                </div>
+                                <div class="carousel-item">
+                                    <i class="fab fa-facebook"></i>
+                                </div>
+                                <div class="carousel-item">
+                                    <i class="fab fa-twitter-square"></i>
+                                </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon px-5" aria-hidden="true"></span>
+                                
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                
+                            </a>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+export default Explore;
