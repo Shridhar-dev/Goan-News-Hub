7406e45a169aa17150bb8cb1c685a7b8a3ffc244
Shridhar-dev <shridharkamat10@gmail.com>
2020-07-01 22:42:51 +0530

    Clean Code

--- src/Know.js
+++ src/Know.js
@@ -14,7 +14,7 @@ class Know extends React.Component{
             <div class="container pt-5" id="knowus">
                 <div class="display-4 pb-3">Who are we 🤔</div>
                 <div class="container h2 textr pb-3">
-                    We are people providing you with the information about Goa and keeping you updated.Apart from the traditional news channel we don't provide the same info but mostly about people and events around Goa.
+                    We are people providing you with the information about Goa and keeping you updated.Apart from the traditional news channel we don't provide the same info but mostly about people and events around Goa.Its unofficial yet.
                 </div>
             </div>
         )
