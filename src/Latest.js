9c21caff2984f7560a54375a057285ecb2aa705e
Shridhar-dev <shridharkamat10@gmail.com>
2020-07-08 16:27:16 +0530

    Clean Code

--- src/Latest.js
+++ src/Latest.js
@@ -22,7 +22,7 @@ class Latest extends React.Component{
                 <h1 class="pt-5 pb-5">Latëst</h1>
                 <div class="row">
                     <div class="col-12 col-xl-4 col-md-6 col-sm-12 pt-2 pb-2 ">
-                        <Cards img={pao} title="Life of Pao" bytitle="By Kabir Naik" text="This video by Kabir Naik tells us about Goa's favourite delicacy the Pao" link="https://www.youtube.com/watch?v=rZcWl7ajSRQ" />
+                        <Cards img="https://i.ytimg.com/vi/qe8cant_b64/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBjDcJoxfdP-UVEByk876BPZVERxQ" title="Render" bytitle="By Kabir Naik" text="The video focusses on  one of goa's traditonal occupation" link="https://www.youtube.com/watch?v=rZcWl7ajSRQ" />
                     </div>
                     <div class="col-12 col-xl-4 col-md-6 col-sm-12 pt-2 pb-2">
                         <Cards img={goaday} title="A Day in a Village of Goa" bytitle="By The Local Bus" text="Experience the feeling of living in a village of Goa" link="https://www.youtube.com/watch?v=klQsVvh_Fic" />
